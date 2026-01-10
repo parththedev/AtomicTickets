885ee2603c260be40c7b16919791f187590aff4b
parththedev <226023719+parththedev@users.noreply.github.com>
2026-01-10 15:47:26 +0530

    Added sync_engine and SyncSessionLocal via sessionmaker

--- src/db/session.py
+++ src/db/session.py
@@ -1,12 +1,21 @@
 from typing import AsyncGenerator
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from src.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True) # echo=True 
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+sync_engine = create_engine(settings.DATABASE_SYNC_URL, echo=True)
+
+SyncSessionLocal = sessionmaker(
+    bind=sync_engine,
     autoflush=False,
     expire_on_commit=False,
 )
