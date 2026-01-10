172613dd4ae446638bca67ef765ddfd247ccbc67
parththedev <226023719+parththedev@users.noreply.github.com>
2026-01-10 15:37:05 +0530

    Added lifespan context to the app for redis_manager to connect and close immediately at the start

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from src.routes import buying_router
+from contextlib import asynccontextmanager
+from src.redis.client import redis_manager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_manager.connect()
+    yield
+    await redis_manager.close()
 
 version = "v1"
 
 app = FastAPI(
+    lifespan=lifespan,
     title="AtomicTickets",
     description="A flash-sale system that handles 1,000 concurrent requests for 10 tickets with zero race conditions or overselling",
     version=version
